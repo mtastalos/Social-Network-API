961caa66fa8b5a302e0bfd514668cc1a941865e6
Michael Astalos <michaeldont@gmail.com>
2021-10-12 23:40:12 -0400

    repurposing code to help set up the api

---
+++ models/Thought.js
@@ -0,0 +1,74 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      require: true,
+      max: 280
+    },
+    username: {
+      type: String,
+      require: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAt => dateFormat(createdAt)
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAt => dateFormat(createdAt)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
